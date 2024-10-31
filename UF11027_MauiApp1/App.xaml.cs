2423b28b285a835151478d926b6a5edae114dc67
tiagomonteirinho <tiagoamonteirinho@gmail.com>
2024-10-31 11:28:03 +0000

    Add grid layouts.

--- UF11027_MauiApp1/App.xaml.cs
+++ UF11027_MauiApp1/App.xaml.cs
@@ -13,7 +13,7 @@
             //navPage.BarTextColor = Colors.Blue;
             //MainPage = navPage;
 
-            MainPage = new NavigationPage(new StackLayout1());
+            MainPage = new NavigationPage(new GridLayout1());
         }
     }
 }
---
+++ UF11027_MauiApp1/GridLayout1.xaml.cs
@@ -0,0 +1,9 @@
+namespace UF11027_MauiApp1;
+
+public partial class GridLayout1 : ContentPage
+{
+	public GridLayout1()
+	{
+		InitializeComponent();
+	}
+}
