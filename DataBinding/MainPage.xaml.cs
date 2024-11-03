238c402ef31f925e8f51d6d5b4a05268d3e2632d
tiagomonteirinho <tiagoamonteirinho@gmail.com>
2024-11-03 17:39:05 +0000

    Add data binding.

---
+++ DataBinding/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DataBinding.Models;
+
+namespace DataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Clicked(object sender, EventArgs e)
+        //{
+        //    var product = new Product
+        //    {
+        //        Name = "Samsung Galaxy A53",
+        //        Price = 579.00M,
+        //        Stock = 1099
+        //    };
+
+        //    //Binding productBinding = new Binding();
+        //    ////productBinding.Source = product.Name; // Replaced by separation.
+        //    //productBinding.Source = product;
+        //    //productBinding.Path = "Name";
+
+        //    //label1.SetBinding(Label.TextProperty, productBinding); // SetBinding([view property], [binding]).
+
+        //    Binding nameBinding = new Binding { Source = product, Path = "Name", StringFormat = "Product: {0}" };
+        //    name_label1.SetBinding(Label.TextProperty, nameBinding);
+
+        //    Binding priceBinding = new Binding { Source = product, Path = "Price", StringFormat = "Price: {0}" };
+        //    price_label1.SetBinding(Label.TextProperty, priceBinding);
+
+        //    Binding stockBinding = new Binding { Source = product, Path = "Stock", StringFormat = "Stock: {0}" };
+        //    stock_label1.SetBinding(Label.TextProperty, stockBinding);
+        //}
+        // Implemented at view.
+    }
+}
